7d22fa2f40522cd114d82d0b6555b5f8e52c37ea
Lewis Morgans <lewis.morgans@chas.co.uk>
2024-09-20 09:45:41 +0100

    Refactor client.lua
    - Add display text when within zone radius
    - Refactor code to optimise system performance
    - Remove vehicles from vehicle list

--- client/client.lua
+++ client/client.lua
@@ -14,29 +14,45 @@ end)
 
 function PlayerWithinSpawnRange()
   local spawnZones = { vector3(490, 13, 69.51), vector3(1857, 3680, 33.79) }
-  local radius = 15
+  local radius = 15.0
   local playerPosition = GetEntityCoords(PlayerPedId())
-  local dstcheck
-  local canActivate = false
 
-  for _, i in ipairs(spawnZones) do
-    dstcheck = GetDistanceBetweenCoords(playerPosition, i)
-    if dstcheck < radius then
-      canActivate = true
+  for _, zoneCoords in ipairs(spawnZones) do
+    local distanceSquared = #(playerPosition - zoneCoords) ^ 2
+    local radiusSquared = radius ^ 2
+
+    if distanceSquared <= radiusSquared then
+      return true
     end
   end
 
-  return canActivate
+  return false
+end
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if PlayerWithinSpawnRange() then
+      DrawTextOnScreen("Press F6 to spawn a vehicle", 0.90, 0.5)     -- Center of the screen
+    end
+  end
+end)
+
+function DrawTextOnScreen(text, x, y)
+  SetTextFont(0)                    -- Set the font
+  SetTextProportional(1)            -- Make the text proportional
+  SetTextScale(0.0, 0.3)            -- Text scale
+  SetTextColour(255, 255, 255, 255) -- Text color (white)
+  SetTextOutline()                  -- Text outline
+  SetTextEntry("STRING")            -- Prepare the text entry
+  AddTextComponentString(text)      -- Add the text to display
+  DrawText(x, y)                    -- Draw the text at coordinates (x, y)
 end
 
 RegisterNUICallback('spawnVehicle', function(data, cb)
   local playerPed = PlayerPedId()
   local pos = GetEntityCoords(playerPed)
   local vehicleName = data
-
-  print('playercoords', pos)
-  print('data sent by react', json.encode(data), data) -- debugPrint
-
   if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
     TriggerEvent('chat:addMessage', {
       args = { vehicleName .. '^* Not found' }
@@ -46,7 +62,7 @@ RegisterNUICallback('spawnVehicle', function(data, cb)
 
   RequestModel(vehicleName)
   while not HasModelLoaded(vehicleName) do
-    Wait(500)
+    Wait(0)
   end
 
   local oldVehicle = GetVehiclePedIsIn(playerPed, true)
